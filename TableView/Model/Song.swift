77d3b9e7b51d645d3a41e4b6d0dcddb19839909c
jasminesung <jasminesung1130@gmail.com>
2024-02-25 14:48:40 -0500

    add Song struct and storyboard changes

--- TableView/Model/Song.swift
+++ TableView/Model/Song.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+struct Song {
+    init(_ desc_param: String, _ artist_param: String, _ year_param: String, _ title_param: String) {
+        description = desc_param
+        artist = artist_param
+        year = year_param
+        title = title_param
+    }
+    var description: String
+    var artist: String
+    var year: String
+    var title: String
+}
